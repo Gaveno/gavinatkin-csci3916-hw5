ec32fa295d2051ad3fa4ef0ac6a72b580fbb65a4
Gavin <gavin.monkeystudios@gmail.com>
2019-04-14 18:25:57 -0600

    Updated Source from Wednesday
    - Updated source code from Wednesday's online lecture.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
+import './App.css';
 import MovieHeader from './components/movieheader';
 import MovieList from './components/movielist';
 import Movie from './components/movie';
-import './App.css';
+import {HashRouter,Route} from 'react-router-dom';
+
+//add routing configuration
 
 class App extends Component {
-    state = {title: null};
+    state = {movieId: null};
 
     handleOnTitleChange = (e) => {
-        this.setState( {
-            title: e
+        this.setState({
+            movieId: e
         });
     }
-
     render() {
         return (
-          <div className="App">
-            <MovieHeader subtitle={this.state.title}/>
-              <MovieList onTitleChange={this.handleOnTitleChange}></MovieList>
-          </div>
+            <div className="App">
+                <HashRouter>
+                    <div>
+                        <MovieHeader movieId={this.state.movieId}/>
+                        <Route exact path="/" render={()=><MovieList onTitleChange={this.handleOnTitleChange} />}/>
+                        <Route path="/movielist" render={()=><MovieList onTitleChange={this.handleOnTitleChange} />}/>
+                        <Route path="/movie" render={()=><Movie onTitleChange={this.handleOnTitleChange}/>}/>
+                    </div>
+                </HashRouter>
+            </div>
         );
     }
 }
